68988b5369fbca2d3713c74d3619fdf5cfc3fda3
Morgan Evans <morgan.evans@twocircles.com>
2025-08-19 14:47:45 +0100

    initial commit 🚀

---
+++ app/api/table/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+import data from "./data.json";
+
+export const GET = () => {
+  return NextResponse.json(
+    {
+      data,
+    },
+    { status: 200 }
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,35 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="flex  min-h-screen  flex-col  items-center  justify-between  p-24">
+      <div className="mb-32 lg:max-w-5xl  lg:w-full lg:mb-0  lg:text-left">
+        <h1 className="mb-3  text-2xl  font-semibold">
+          React Pair Programming Task
+        </h1>
+        <p className="mb-5">
+          Using the{" "}
+          <a href="/api/table" target="_blank">
+            http://localhost:3000/api/table
+          </a>{" "}
+          endpoint, we’d like you to fetch the league standings and display them
+          in a table.
+        </p>
+        <h2 className="mb-3 text-2xl font-semibold">Requirements</h2>
+        <p className="mb-5">Display the following for each team:</p>
+        <ul className="list-disc  pl-4  pb-3">
+          <li>Team name</li>
+          <li>Goals for</li>
+          <li>Goals against</li>
+          <li>Points</li>
+        </ul>
+        <p className="mb-5">
+          Build the ability to sort the table by points, goals for, goals
+          against and alphabetically by name.
+        </p>
+        <p>
+          <strong>Bonus</strong> - If we have time, paginate the table by groups
+          of five.
+        </p>
+      </div>
+    </main>
   );
 }
